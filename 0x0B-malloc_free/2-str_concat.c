09ef5a36c8928de62715529b78ce559bda6d732f
Azingya Cosmos <azingyacosmos22578@gmail.com>
2023-01-10 02:22:47 -0800

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,11 +31,17 @@ char *str_concat(char *s1, char *s2)
 	char *dst;
 	unsigned int i, j, size;
 
+	/*If the array is empty*/
 	if (s1 == NULL)
 		s1 = "";
 
+	if (s2 == NULL)
+		s2 = "";
+
+	/*count size total*/
 	size = (_strlen(s1) + _strlen(s2) + 1);
 
+	/*malloc*/
 	dst = (char *) malloc(size * sizeof(char));
 
 	if (dst == 0)
@@ -43,7 +49,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	/* concatenate arrays */
+	/*Concatenate arrays*/
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(dst + i) = *(s1 + i);
 
