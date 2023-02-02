583d99c14b5fb0338563caaca3b97495e32ad0af
azingya cosmos <azingyacosmos22578@gmail.com>
2023-02-02 11:36:50 -0800

    110

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,13 +4,14 @@
  * flip_bits - number of bits you would need to flip
  * to get from one number to another
  * @n: parameter
- *  @m: destiny
- *  Return: nflips
+ * @m: destiny
+ * Return: nflips
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i, nflips = 0;
+	unsigned long int j = sizeof(unsigned long int) * 8;
 
 	for (i = 0; i < j; i++)
 	{
@@ -21,4 +22,3 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	}
 	return (nflips);
 }
-	
